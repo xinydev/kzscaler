b03bc532ccd0a893eabea1727679bc63049947b3
XinYang <xinydev@gmail.com>
2022-02-20 10:46:39 +0800

    feat(proxy): using gin for server
    Signed-off-by: XinYang <xinydev@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kzscaler/kzscaler
 go 1.17
 
 require (
+	github.com/gin-gonic/gin v1.7.7
 	go.uber.org/zap v1.19.1
 	k8s.io/api v0.22.5
 	k8s.io/apimachinery v0.22.5
@@ -26,12 +27,16 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful v2.9.5+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-kit/log v0.1.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.0 // indirect
 	github.com/go-logr/logr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -44,7 +49,9 @@ require (
 	github.com/imdario/mergo v0.3.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -56,10 +63,12 @@ require (
 	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/prometheus/statsd_exporter v0.21.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/automaxprocs v1.4.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63 // indirect
 	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- pkg/scheduler/simple_scheduler.go
+++ pkg/scheduler/simple_scheduler.go
@@ -2,8 +2,12 @@ package scheduler
 
 import (
 	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 	"strings"
+	"time"
 )
 
 // Scheduler tell kzscaler-proxy what to do
@@ -14,53 +18,54 @@ type Scheduler interface {
 }
 
 type SimpleScheduler struct {
-	enabledService map[string]int
+	services map[string]int // zero-scale feature enabled services
+	router   *gin.Engine
+	logger   *zap.SugaredLogger
 }
 
 type SimpleSchedulerArgs struct {
 }
 
 func NewScheduler() Scheduler {
+	l, _ := zap.NewDevelopment()
 	return &SimpleScheduler{
-		enabledService: map[string]int{},
+		services: map[string]int{},
+		router:   gin.Default(),
+		logger:   l.Sugar(),
 	}
 }
 
 // Start
 // This is a blocking call.
 func (s *SimpleScheduler) Start(ctx context.Context) error {
-	http.HandleFunc("/enabled", s.getEnabledServiceHandler)
-	http.HandleFunc("/zerostate", s.getZeroStateHandler)
-	return http.ListenAndServe(":8080", nil)
+	s.router.GET("/service", s.getServicesHandler)
+	s.router.GET("/scale_up/:service", s.scaleUpHandler)
+
+	return s.router.Run(":8080")
 }
 
 func (s *SimpleScheduler) AddService(name string, replicate int) {
-	s.enabledService[name] = replicate
+	s.services[name] = replicate
 
 }
 
 func (s *SimpleScheduler) UpdateReplicas(name string, replicate int) {
-	s.enabledService[name] = replicate
+	s.services[name] = replicate
 }
 
-func (s *SimpleScheduler) getZeroStateHandler(w http.ResponseWriter, req *http.Request) {
-	services := make([]string, 0)
-	for k, v := range s.enabledService {
-		if v == 0 {
-			services = append(services, k)
-		}
-	}
-	w.WriteHeader(231)
-	_, _ = w.Write([]byte(strings.Join(services, "|")))
+func (s *SimpleScheduler) getServicesHandler(c *gin.Context) {
 
+	services := make([]string, 0)
+	for k, v := range s.services {
+		services = append(services, fmt.Sprintf("%s%%s", k, v))
+	}
+
+	c.String(http.StatusOK, strings.Join(services, "&"))
 }
-func (s *SimpleScheduler) getEnabledServiceHandler(w http.ResponseWriter, req *http.Request) {
-	services := make([]string, 0)
-	for k, _ := range s.enabledService {
-		services = append(services, k)
 
-	}
-	w.WriteHeader(231)
-	_, _ = w.Write([]byte(strings.Join(services, "|")))
-
+func (s *SimpleScheduler) scaleUpHandler(c *gin.Context) {
+	name := c.Param("name")
+	s.logger.Infof("service need scale up,%s", name)
+	time.Sleep(3 * time.Second)
+	c.String(http.StatusOK, "")
 }
